cd6d6efa2b2eb2b63bfbb91c5557437dbcbff062
azamat <azamatnematov2003@gmail.com>
2022-06-13 23:25:15 +0500

    create service, dto, filter, repository.....

--- src/main/java/com/example/market/model/azamat/Vehicles.java
+++ src/main/java/com/example/market/model/azamat/Vehicles.java
@@ -15,4 +15,36 @@ public class Vehicles {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
+    @Column(name = ("merchant_id"))
+    private Integer merchantId;
+
+    @Column(name = ("brand_id"))
+    private Integer branId;
+
+    @Column(name = ("model_id"))
+    private Integer modelId;
+
+    @Column(name = ("body_type_id"))
+    private Integer bodyTypeId;
+
+    private String name;
+    private Integer year;
+    private String color;
+    private Double price;
+
+    @Column(name = ("number_sits"))
+    private Integer numberSits;
+
+    @Column(name = ("is_new"))
+    private Boolean isNew;
+    private Boolean status;
+
+    @Column(name = ("condition_duration_type"))
+    private Integer conditionDurationType;
+
+    @Column(name = ("condition_duration"))
+    private Integer condition_duration;
+
+    @Column(name = ("condition_price"))
+    private Double conditionPrice;
 }
